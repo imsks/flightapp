4bb9ad1a4b822040ca86d1153e51e1d71b4c5655
sachin <sachinkshuklaoo7@gamil.com>
2020-03-10 00:35:45 +0530

    PopUpButton widget implemented

---
+++ lib/constants.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+// Theme
+ThemeData appTheme = ThemeData(
+  primaryColor: Color(0xFFF3791A),
+  fontFamily: 'Oxygen'
+);
+
+
+// Colors
+const Color kFirstColor = Color(0xFFF47D15);
+const Color kSecondColor = Color(0xFFEF772C);
+const kWhiteColor = Colors.white;
+
+
+// Other content
+List<String> locations = ['Boston (BOS)', 'New York City (JFK)'];
+
+const TextStyle dropDownLabelStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 16
+);
+
+const TextStyle dropDownMenuItemStyle = TextStyle(
+    color: Colors.black,
+    fontSize: 16
+);
